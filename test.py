9ddfcc0cd636e17ff30af5f97a5dde1747a3b5b4
komail11 <64306088+komail11@users.noreply.github.com>
2021-02-14 22:44:20 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,61 @@
+import random
+import time 
+#Creating title
+text = "Rock Paper Scissors"
+title = text.center(225,"#")
+print(title)
+
+#Implementing game
+def rps ():
+    rnd = int(input("How many rounds of games do you want to play ?\n"))
+    three_choices = ["Rock","Paper","Scissors"]
+    count_w = 0
+    count_l = 0
+
+    while True:
+        if count_w == rnd or count_l == rnd:
+            break
+        user = input("Input: ")
+        ai = random.choice(three_choices)
+        print("AI: ",ai)
+    
+        if user == "Rock":
+            if ai == user:
+                pass
+            elif ai == "Paper":
+                count_l += 1
+            elif ai == "Scissors":
+                count_w += 1
+
+        if user == "Paper":
+            
+            if ai == user:
+                pass
+            elif ai == "Rock":
+                count_w += 1
+            elif ai == "Scissors":
+                count_l += 1
+            
+        if user == "Scissors":
+            if ai == user:
+                pass
+            elif ai == "Rock":
+                count_l += 1      
+            elif ai == "Paper":
+                count_w += 1
+               
+
+    if count_w > count_l:
+        print("User has won")
+    else:
+        print("AI has won")
+
+rps()
+
+#Rematch
+rematch = input("Would you like to play again ? \n")
+if rematch == "Yes":
+    rps()
+elif rematch == "No":
+    quit()
+
